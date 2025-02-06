2404b47ab317f4bfdb6bee2af1b3d099579c5f82
viper23 <htonnycarlos@gmail.com>
2025-02-06 17:28:14 +0200

    wip on signup

--- lib/feature/auth/screens/auth_screen.dart
+++ lib/feature/auth/screens/auth_screen.dart
@@ -1,13 +1,15 @@
 import 'package:amazon_clone/common/widgets/costum_button.dart';
 import 'package:amazon_clone/common/widgets/custom_text_field.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/feature/auth/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-enum Auth{
+enum Auth {
   signup,
   signin,
 }
+
 class AuthScreen extends StatefulWidget {
   static const String routeName = "/auth-screen";
   const AuthScreen({super.key});
@@ -20,6 +22,9 @@ class _AuthScreenState extends State<AuthScreen> {
   Auth _auth = Auth.signup;
   final signUpFormKey = GlobalKey<FormState>();
   final signInFormKey = GlobalKey<FormState>();
+
+  // for backend integration
+  final AuthService authService = AuthService();
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -34,51 +39,117 @@ class _AuthScreenState extends State<AuthScreen> {
     super.dispose();
   }
 
+  void signUpUser() {
+    authService.signupUser(
+        context: context,
+        name: _nameController.text,
+        email: _emailController.text,
+        password: _passwordController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: GlobalVariables.greyBackgroundColor,
-      body: SafeArea(child: Column(
+      body: SafeArea(
+          child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Center(child: Text("Welcome", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),)),
-          ListTile(tileColor : _auth == Auth.signup ? GlobalVariables.backgroundColor : GlobalVariables.greyBackgroundColor ,title: Text("Create account", style: TextStyle(fontWeight: FontWeight.bold),),leading: Radio(activeColor: GlobalVariables.secondaryColor, value: Auth.signup, groupValue: _auth, onChanged: (Auth? value) { setState(() {
-            _auth = value!;
-          }); },),),
-
+          Center(
+              child: Text(
+            "Welcome",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+          )),
+          ListTile(
+            tileColor: _auth == Auth.signup
+                ? GlobalVariables.backgroundColor
+                : GlobalVariables.greyBackgroundColor,
+            title: Text(
+              "Create account",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            leading: Radio(
+              activeColor: GlobalVariables.secondaryColor,
+              value: Auth.signup,
+              groupValue: _auth,
+              onChanged: (Auth? value) {
+                setState(() {
+                  _auth = value!;
+                });
+              },
+            ),
+          ),
           if (_auth == Auth.signup)
             Container(
               padding: EdgeInsets.all(8),
               color: GlobalVariables.backgroundColor,
               child: Form(
-                key: signUpFormKey, child: Column(children: [
-                  CustomTextField(controller: _emailController, hintText: "Email",),
+                key: signUpFormKey,
+                child: Column(children: [
+                  CustomTextField(
+                    controller: _emailController,
+                    hintText: "Email",
+                  ),
                   Gap(10),
-                  CustomTextField(controller: _nameController, hintText: "Name",),
+                  CustomTextField(
+                    controller: _nameController,
+                    hintText: "Name",
+                  ),
                   Gap(10),
-                  CustomTextField(controller: _passwordController, hintText: "Password",),
+                  CustomTextField(
+                    controller: _passwordController,
+                    hintText: "Password",
+                  ),
                   Gap(30),
-                  CustomButton(text: "Sing up", onTap: (){}),
-              ]),
+                  CustomButton(
+                      text: "Sing up",
+                      onTap: () {
+                        if (signUpFormKey.currentState!.validate()) {
+                          signUpUser();
+                        }
+                        signUpUser();
+                      }),
+                ]),
               ),
             ),
-
-          ListTile(tileColor : _auth == Auth.signin ? GlobalVariables.backgroundColor : GlobalVariables.greyBackgroundColor ,title: Text("Login", style: TextStyle(fontWeight: FontWeight.bold),),leading: Radio(activeColor: GlobalVariables.secondaryColor, value: Auth.signin, groupValue: _auth, onChanged: (Auth? value) { setState(() {
-            _auth = value!;
-          }); },),),
-
+          ListTile(
+            tileColor: _auth == Auth.signin
+                ? GlobalVariables.backgroundColor
+                : GlobalVariables.greyBackgroundColor,
+            title: Text(
+              "Login",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            leading: Radio(
+              activeColor: GlobalVariables.secondaryColor,
+              value: Auth.signin,
+              groupValue: _auth,
+              onChanged: (Auth? value) {
+                setState(() {
+                  _auth = value!;
+                });
+              },
+            ),
+          ),
           if (_auth == Auth.signin)
             Container(
               padding: EdgeInsets.all(8),
               color: GlobalVariables.backgroundColor,
               child: Form(
-                key: signUpFormKey, child: Column(children: [
-                CustomTextField(controller: _emailController, hintText: "Email",),
-                Gap(10),
-                CustomTextField(controller: _passwordController, hintText: "Password",),
-                Gap(30),
-                CustomButton(text: "Login", onTap: (){}),
-              ]),
+                key: signUpFormKey,
+                child: Column(children: [
+                  CustomTextField(
+                    controller: _emailController,
+                    hintText: "Email",
+                  ),
+                  Gap(10),
+                  CustomTextField(
+                    controller: _passwordController,
+                    hintText: "Password",
+                  ),
+                  Gap(30),
+                  CustomButton(text: "Login", onTap: () {}),
+                ]),
               ),
             ),
         ],
